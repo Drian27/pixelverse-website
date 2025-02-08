f2cf35dbce54d9dca037f8b0eca7d2a153e1512e
Drian27 <muhamaddrian05@gmail.com>
2025-02-08 14:07:49 +0700

    push

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,14 @@
 import About from '@/components/About'
 import React from 'react'
+import Partner from '@/components/Partner';
+import History from '@/components/History';
 
 export default function page() {
   return (
     <div>
       <About />
+      <History />
+      <Partner />
     </div>
   )
 }
---
+++ src/components/History/index.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Index() {
+  return (
+    <div className="flex flex-col items-center p-4 md:p-10 bg-black min-h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-4xl">
+        <div className="bg-white p-4 rounded-2xl shadow-lg flex items-center">
+          <Image
+            src="/box.png"
+            width={300}
+            height={300}
+            alt="Photobooth 1"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="bg-white p-4 rounded-2xl shadow-lg flex items-center">
+          <Image
+            src="/box.png"
+            width={300}
+            height={300}
+            alt="Photobooth 2"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="bg-white p-4 rounded-2xl shadow-lg flex items-center">
+          <Image
+            src="/box.png"
+            width={300}
+            height={300}
+            alt="Photobooth 3"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="bg-white p-4 rounded-2xl shadow-lg flex items-center">
+          <Image
+            src="/box.png"
+            width={300}
+            height={300}
+            alt="Photobooth 3"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="bg-white p-4 rounded-2xl shadow-lg flex items-center">
+          <Image
+            src="/box.png"
+            width={300}
+            height={300}
+            alt="Photobooth 3"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="bg-white p-4 rounded-2xl shadow-lg flex items-center">
+          <Image
+            src="/box.png"
+            width={300}
+            height={300}
+            alt="Photobooth 3"
+            className="rounded-xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
