05d02308461afc656f876f8a9d442ace4b77a7f6
haikalll27 <muhammadhaikalsch@gmail.com>
2025-02-10 01:37:34 +0700

    push new

--- src/components/Common/BgPixelVerse.tsx
+++ src/components/Common/BgPixelVerse.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function BgPixelVerse() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null; // Mencegah render di server
+
   return (
     <section className="w-full h-full">
       <div>
@@ -13,7 +22,6 @@ export default function BgPixelVerse() {
           height={1080}
           quality={100}
           className="w-full h-auto object-cover"
-          // style={{ marginTop: "50px" }}
         />
       </div>
     </section>
